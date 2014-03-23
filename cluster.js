f68a548082309798b590532e971f1c74c311b814
Andrew Barba <abarba.77@gmail.com>
2014-03-23 11:32:24 -0400

    add cluster and server

---
+++ cluster.js
@@ -0,0 +1,24 @@
+
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+
+    // check number of cpu's
+    var cpuCount = require('os').cpus().length;
+    console.log('CPUs: '+cpuCount);
+    
+    // fork process for each cpu
+    for (var i = 0; i < cpuCount; i++) {
+        cluster.fork();
+    }
+
+    // handle failures
+    cluster.on('exit', function (worker) {
+        console.log('Worker ' + worker.id + ' crashed');
+        cluster.fork();
+    });
+} else {
+
+    // run server
+    require('./app')();
+}
---
+++ server.js
@@ -0,0 +1,3 @@
+
+// start the app
+require('./app')();
