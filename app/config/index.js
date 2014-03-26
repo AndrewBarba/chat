9552938aef0e403a57a74860bde42961e9c56bdb
Andrew Barba <abarba.77@gmail.com>
2014-03-26 11:20:35 -0400

    read from env

--- app/config/index.js
+++ app/config/index.js
@@ -2,7 +2,7 @@
 var _ = require('underscore');
 
 var prod = {
-    db: 'mongodb://api:chat-api-password@oceanic.mongohq.com:10079/chat',
+    db: process.env.DB,
     twilio: {
         sid: 'AC444de0a850e8d0e457e3f8ae5b9e332b',
         token: '1dc58f02c00de870bbfd553826c7bf56',
@@ -11,7 +11,7 @@ var prod = {
 }
 
 var dev = {
-    db: 'mongodb://api:chat-api-password@oceanic.mongohq.com:10079/chat',
+    db: process.env.DB || 'mongodb://api:chat-api-password@oceanic.mongohq.com:10079/chat',
     twilio: {
         sid: 'AC444de0a850e8d0e457e3f8ae5b9e332b',
         token: '1dc58f02c00de870bbfd553826c7bf56',
